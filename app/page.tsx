c9228ee2bc4ca76b2c99f460c17bbd3de47c05db
SamKTA <150455266+SamKTA@users.noreply.github.com>
2025-09-24 16:48:24 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import VisitForm from '../components/VisitForm'
+import ObservationForm from '../components/ObservationForm'
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-gray-50">
+    <main className="min-h-screen bg-gray-50 pb-20">
       <VisitForm />
+      <ObservationForm />
     </main>
   )
 }
