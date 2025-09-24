d9f87cea11d78fd438757a40440867d6916dd3a0
SamKTA <150455266+SamKTA@users.noreply.github.com>
2025-09-24 17:10:36 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,5 @@
 import VisitForm from '../components/VisitForm'
 import ObservationForm from '../components/ObservationForm'
-
-export default function Home() {
-  return (
-    <main className="min-h-screen bg-gray-50 pb-20">
-      <VisitForm />
-      <ObservationForm />
-    </main>
-  )
-}
-
 import SignaturePad from '../components/SignaturePad'
 
 export default function Home() {
