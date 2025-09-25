146e9afc84074860e6ee31724e80b85fdbbbb435
SamKTA <150455266+SamKTA@users.noreply.github.com>
2025-09-25 09:51:30 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,28 @@
+'use client'
+
+import { useState } from 'react'
 import VisitForm from '../components/VisitForm'
 import ObservationForm from '../components/ObservationForm'
 import SignaturePad from '../components/SignaturePad'
 import GeneratePDF from '../components/GeneratePDF'
 
 export default function Home() {
+  const [visitData, setVisitData] = useState({
+    date: '',
+    address: '',
+    redacteur: '',
+    arrivalTime: '',
+    departureTime: '',
+    buildingCode: '',
+    personnesPresentes: ''
+  })
+
   return (
-    <main className="min-h-screen bg-gray-50 pb-20 px-4">
-      <div className="max-w-3xl mx-auto space-y-10 py-10">
-        {/* Formulaire de visite */}
-        <VisitForm />
-
-        {/* Observations */}
-        <ObservationForm />
-
-        {/* Signature */}
-        <SignaturePad />
-
-        {/* Bouton pour générer le PDF */}
-        <GeneratePDF />
-      </div>
+    <main className="min-h-screen bg-gray-50 pb-20 px-4 space-y-10">
+      <VisitForm visitData={visitData} setVisitData={setVisitData} />
+      <ObservationForm />
+      <SignaturePad />
+      <GeneratePDF visitData={visitData} />
     </main>
   )
 }
