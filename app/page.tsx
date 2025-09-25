0ff4263216f3eada88cf2758fe4528e06f1da21b
SamKTA <150455266+SamKTA@users.noreply.github.com>
2025-09-25 09:26:59 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,24 @@
 import VisitForm from '../components/VisitForm'
 import ObservationForm from '../components/ObservationForm'
 import SignaturePad from '../components/SignaturePad'
+import GeneratePDF from '../components/GeneratePDF'
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-gray-50 pb-20">
-      <VisitForm />
-      <ObservationForm />
-      <SignaturePad />
+    <main className="min-h-screen bg-gray-50 pb-20 px-4">
+      <div className="max-w-3xl mx-auto space-y-10 py-10">
+        {/* Formulaire de visite */}
+        <VisitForm />
+
+        {/* Observations */}
+        <ObservationForm />
+
+        {/* Signature */}
+        <SignaturePad />
+
+        {/* Bouton pour générer le PDF */}
+        <GeneratePDF />
+      </div>
     </main>
   )
 }
